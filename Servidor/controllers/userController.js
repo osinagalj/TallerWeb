3688aa299e6f51c8349ce7bfd75ba68bbf69298c
Lautaro Osinaga <49956395+osinagalj@users.noreply.github.com>
2021-05-29 12:05:31 -0400

    users methods

--- Servidor/controllers/userController.js
+++ Servidor/controllers/userController.js
@@ -16,3 +16,80 @@ exports.createUser = async (req,res) => {
     }
     
 }
+
+exports.getUsers = async (req,res) => {
+    console.log(req.body);
+    try{
+        const users = await User.find();
+        res.json(users)
+    
+    }catch(error){
+        console.log(error);
+        res.status(500).send('HUbo un error');
+    }
+}
+
+
+exports.updateUser = async (req,res) => {
+    console.log(req.body);
+    try{
+
+        const { name, email, phone, date} = req.body;
+        let user = await User.findById(req.params.id);
+        
+        if(!user){
+            res.status(404).send('No existe el producto');
+        }
+
+        user.name = name;
+        user.email = email;
+        user.phone = phone;
+        user.date = date;
+
+        user = await User.findOneAndUpdate({ _id: req.params.id}, user, {new: true});
+
+        res.json(user);
+
+    }catch(error){
+        console.log(error);
+        res.status(500).send('HUbo un error');
+    }
+    
+}
+
+
+exports.getUser = async (req,res) => {
+    console.log(req.body);
+    try{
+        let user = await User.findById(req.params.id);
+
+        if(!user){
+            res.status(404).send('No existe el usuario');
+        }
+
+        res.json(user);
+
+    }catch(error){
+        console.log(error);
+        res.status(500).send('HUbo un error');
+    }
+}
+
+
+exports.deleteUser = async (req, res) => {
+
+    try {
+        let user = await User.findById(req.params.id);
+
+        if(!user) {
+            res.status(404).json({ msg: 'No existe el usuario' })
+        }
+       
+        await User.findOneAndRemove({ _id: req.params.id })
+        res.json({ msg: 'User eliminado con exito' });
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+}
--- Servidor/index.js
+++ Servidor/index.js
@@ -15,14 +15,9 @@ app.use(cors());
 
 */
 app.use(express.json());
-app.use('/api/productos',require('./routes/producto'))
+
 app.use('/api/users',require('./routes/user'))
 
-/*
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-*/
 app.listen(4000, () => {
     console.log('El servidor esta corriendo perfectamente');
 })
